56d252a82001f4729ba40d806ad369f3f0df20b2
Mohammed Hamid Hamoda <hamoda.dev@gmail.com>
2022-06-07 07:14:21 +0200

    Time: 40 ms (24.39%), Space: 20.4 MB (78.05%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Integer
+     */
+    function peakIndexInMountainArray($arr) {
+        $left = 0;
+        $right = count($arr) - 1;
+        
+        while ($left < $right) {
+            $mid = floor(($left + $right) / 2);
+            
+            if ($arr[$mid] < $arr[$mid + 1]) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid;
+            }
+        }
+        
+        return $left;
+    }
+}
